e8aa6d56d7cd8fc0165f75a3781efc315ed6712d
Lukasz <siennicki.lukasz95@gmail.com>
2020-06-21 11:09:50 +0200

    zadanie kalkulator, wyjatki

--- src/main/java/zadanie/Calculator.java
+++ src/main/java/zadanie/Calculator.java
@@ -1,33 +1,7 @@
 package zadanie;
 
 public class Calculator {
-    private int a;
-    private int b;
 
-    public Calculator(){
-
-    }
-
-    public Calculator(int a, int b) {
-        this.a = a;
-        this.b = b;
-    }
-
-    public int getA() {
-        return a;
-    }
-
-    public void setA(int a) {
-        this.a = a;
-    }
-
-    public int getB() {
-        return b;
-    }
-
-    public void setB(int b) {
-        this.b = b;
-    }
 
     public double divide(int a, int b) throws DivideByZeroException {
         if (b == 0) {
